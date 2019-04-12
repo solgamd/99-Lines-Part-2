527ea38f9f5e831289df4e26a9d1555460858042
solgamd <solgamd@gmail.com>
2019-04-12 16:29:02 +0200

    Reconnecting local repo to remote repo 99LinesPart2

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-let friends = ['Niko','Emilie','Marj','Anna','Maria'];
+let friends = ['Niko', 'Emilie', 'Marj', 'Anna', 'Maria'];
 
-for (let i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
-    for (let n = 99; n > 1; n--) {
-        console.log(n + " lines of code in the file, " + n + " lines of code, " + friends[i] + " strikes one out, clears it all out, " + [n-1] + " lines of code in the file");
-    } if (n === 1) {
-        console.log(n + " line of code in the file, " + n + " line of code, " + friends[i] + " strikes one out, clears it all out, " + "no more lines of code in the file");
-    } else if (n < 1) {
-        break;
+let btn = document.createElement('button');
+document.body.appendChild(btn);
+btn.innerText = 'Sing!'
+
+btn.addEventListener('click', sing);
+
+function sing() {
+
+    for (let i = 0; i < friends.length; i++) {
+        let container = document.createElement('container');
+        container.class = 'friend';
+        document.body.appendChild(container);
+
+        let h3 = document.createElement('h3');
+        let h3Text = document.createTextNode(friends[i]);
+        h3.appendChild(h3Text);
+        container.appendChild(h3);
+
+        let div = document.createElement('div');
+        container.appendChild(div);
+        let p = document.createElement('paragraph');
+        div.appendChild(p);
+
+        for (let n = 99; n >= 1; n--) {
+            if (n >= 2) {
+                let pTextAll = document.createTextNode(n + " lines of code in the file, " + n + " lines of code, " + friends[i] + " strikes one out, clears it all out, " + [n - 1] + " lines of code in the file.");
+                p.appendChild(pTextAll);
+
+            } else {
+                let pTextLast = document.createTextNode(n + " line of code in the file, " + n + " line of code, " + friends[i] + " strikes one out, clears it all out, " + "no more lines of code in the file.");
+                p.appendChild(pTextLast);
+            }
+        }
     }
 }
